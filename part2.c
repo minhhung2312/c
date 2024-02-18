5e53575bf6a525783809e717921413e19ccf4591
minhhung2312 <vmh26122004@gmail.com>
2024-02-18 23:24:50 +0700

    c

--- part2.c
+++ part2.c
@@ -1,4 +1,3 @@
-//HAHA
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -204,5 +203,164 @@
 // }
 
 //BAI 32:
-//SO THUAN NGHICH CO 3 UOC NGUYEN TO KHAC NHAU:
+//SO THUAN NGHICH VA CO IT NHAT 3 UOC NGUYEN TO KHAC NHAU:
+//UOC SO NGUYEN TO --> PHAN TICH THUA SO NGUYEN TO:
+//CACH DON GIAN:
+// int reversibleNumber(int n){
+//     int temp=n;
+//     int sum=0;
+//     while(n){
+//         sum=sum*10+n%10;
+//         n/=10;
+//     }
+//     return sum==temp;
+// }
+// int prime(int n){
+//     for(int i=2; i<=sqrt(n); i++){
+//         if(n%i==0) return 0;
+//     }
+//     return n>1;
+// }
+// int divisor(int n){
+//     int cnt=0;
+//     for(int i=1; i<=n; i++){
+//         if(n%i==0 && prime(i)) ++cnt;
+//     }
+//     return cnt>=3;
+// }
+// int main(){
+//     int l,r;
+//     scanf("%d%d",&l, &r);
+//     for(int i=l; i<=r; i++){
+//         if(reversibleNumber(i) && divisor(i))
+//             printf("%d ",i);
+//     }
+// }
 
+//CACH NHANH HON:
+// int reversibleNumber(int n){
+//     int sum=0, temp=n;
+//     while(n){
+//         sum=sum*10+n%10;
+//         n/=10;
+//     }
+//     return sum==temp;
+// }
+// int divisor(int n){
+//     int cnt=0;
+//     for(int i=2; i<=sqrt(n); i++){
+//         if(n%i==0){
+//             ++cnt;
+//             while(n%i==0){
+//                 n/=i;
+//             }
+//         }
+//     }
+//     if(n!=1) ++cnt;
+//     return cnt>=3;
+// }
+// int main(){
+//     int l,r;
+//     scanf("%d%d",&l,&r);
+//     for(int i=l; i<=r; i++){
+//         if(reversibleNumber(i) && divisor(i))
+//             printf("%d ",i);
+//     }
+// }
+
+//BAI 33:
+//DEM CHU SO CHAN,LE:
+// void oddEvenNumber(int n){
+//     int cntOdd=0, cntEvenNumber=0;
+//     while(n){
+//         int temp=n%10;
+//         if(temp%2==0)
+//             cntEvenNumber++;
+//         else
+//             cntOdd++;
+//         n/=10;
+//     }
+//     printf("%d %d",cntOdd, cntEvenNumber);
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     oddEvenNumber(n);
+// }
+
+//SO STRONG:
+//SO STRONG LA SO CO TONG GIAI THUA CAC CHU SO BANG CHINH NO:
+//VI DU: 145 = 1! + 4! + 5! = 1 + 24 + 120 = 145 LA SO STRONG.
+// int factorial(int n){
+//     int res=1;
+//     for(int i=1; i<=n; i++){
+//         res*=i;
+//     }
+//     return res;
+// }
+// int strongNumber(int n){
+//     int sum=0, temp=n;
+//     while(n){
+//         sum+=factorial(n%10);
+//         n/=10;
+//     }
+//     return sum==temp;
+// }
+// int main(){
+//     int l,r;
+//     scanf("%d%d",&l,&r);
+//     for(int i=l; i<=r; i++){
+//         if(strongNumber(i))
+//             printf("%d ",i);
+//     }
+// }
+
+//BAI 34:
+//SO LOC PHAT:
+//SO LOC PHAT LA SO CHI CHU CAC SO 0,6,8:
+// int fortune(int n){
+//     while(n){
+//         int temp=n%10;
+//         if(temp!=0 && temp!=6 && temp!=8) return 0;
+//         n/=10;
+//     }
+//     return 1;
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     printf("%d",fortune(n));
+// }
+
+//SO THUAN NGHICH VA LOC PHAT:
+//LA SO THUAN NGHICH, CO CHUA IT NHAT MOT CHU SO 6,
+//TONG CAC CHU SO CUA NO CO CHU SO CUOI CUNG LA 8:
+int reversibleNumber(int n){
+    int sum=0, temp=n;
+    while(n){
+        sum=sum*10+n%10;
+        n/=10;
+    }
+    return sum==temp;
+}
+int check(int n){
+    int ok=0;
+    int sum=0;
+    while(n){
+        int r=n%10;
+        sum+=r;
+        if(r==6) ok=1;
+        n/=10;
+    }
+    if(ok && sum%10==8) return 1;
+    return 0;
+}
+int main(){
+    int l,r;
+    scanf("%d%d",&l,&r);
+    for(int i=l; i<=r; i++){
+        if(check(i) && reversibleNumber(i)){
+            printf("%d ",i);
+        }
+    }
+}
